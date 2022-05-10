81e4385f4f4fdda4e3861bc627a0b62aa70b7e5c
newhastag <hashtagkewl.mail@gmail.com>
2022-05-10 13:56:51 +0200

    add code challenge

---
+++ CC20220509/app.js
@@ -0,0 +1,20 @@
+/*
+# TASK:
+Create a method all which takes two params:
+
+    a sequence
+    a function (function pointer in C)
+
+and returns true if the function in the params returns true for every element in the sequence. 
+Otherwise, it should return false. If the sequence is empty, it should return true, since technically nothing failed the test.
+
+# EXAMPLE:
+all((1, 2, 3, 4, 5), greater_than_9) -> false
+all((1, 2, 3, 4, 5), less_than_9)    -> True
+all([1,2,3,4,5], function(v){return v<9}) -> true
+all([1,2,3,4,5], function(v){return v>9}) -> false
+*/
+
+function all(arr,fun){
+  return arr.every(fun)
+}
